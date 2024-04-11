6648b8ba705596fa3164396ebef7560550c2aaad
sanstha1 <sthasantosh070@gmail.com>
2024-04-11 10:49:20 +0545

    Task 3 of week 1 has been completed.

---
+++ Week1/task3.java
@@ -0,0 +1,15 @@
+package Task.Week1;
+
+public class task3 {
+    public static void main(String[] args){
+        /*Write a program that prints the following output:
+           A "quoted" String is
+           'much' better if you learn
+           the rules of "escape sequences."
+           Also, "" represents an empty
+           String. Don't forget: use \"
+           instead of " !
+           '' is not the same as " */
+        System.out.println("A \"quoted\" String is \'much\' better if you learn the rules of \"escape sequences.\" \nAlso, \"\" represents an emptyString. Don't forget: use \\\" instead of \" !\'\' is not the same as\"");
+    }
+}
