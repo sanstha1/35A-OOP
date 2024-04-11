446e6ad78ed4094debd913e80f0e649924f770bd
sanstha1 <sthasantosh070@gmail.com>
2024-04-11 10:22:00 +0545

    Modified or Extended the above code

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
     public static void main(String[] args){
         /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World!");
+        System.out.print("This is my first program\n");
+        System.out.print("I am on module ST4003CEM\n");
     }
 }
+
+
