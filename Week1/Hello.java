6d467a9159c108884a613a1a648ddb8133d7ae2b
sanstha1 <sthasantosh070@gmail.com>
2024-04-11 10:11:34 +0545

    Week 1 started.

---
+++ Week1/Hello.java
@@ -0,0 +1,8 @@
+package Task.Week1;
+
+public class Hello {
+    public static void main(String[] args){
+        /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
+        System.out.println("Hello World!");
+    }
+}
