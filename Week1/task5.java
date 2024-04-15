0c13f38ab591677c93e2bf6e5de97337d88df343
sanstha1 <sthasantosh070@gmail.com>
2024-04-15 07:35:10 +0545

    Task4 and Task5 of week1 added.

---
+++ Week1/task5.java
@@ -0,0 +1,10 @@
+package Task.Week1;
+
+public class task5 {
+    public static void main(String[] args){
+        System.out.print("********* \n");
+        System.out.print("********* \n");
+        System.out.print("********* \n");
+        System.out.print("********* \n");
+    }
+}
