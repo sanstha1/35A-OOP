000c71c86a497ce6f6d2966cedb053889a32128d
sanstha1 <sthasantosh070@gmail.com>
2024-04-15 07:39:23 +0545

    Question has been added on Week 4 and week 5

--- Week1/task4.java
+++ Week1/task4.java
@@ -2,6 +2,13 @@ package Task.Week1;
 
 public class task4 {
     public static void main(String[] args){
+
+        /*WAP that prints the following pattern:
+         *
+         **
+         ***
+         **** */
+        
         System.out.print("* \n");
         System.out.print("** \n");
         System.out.print("*** \n");
--- Week1/task5.java
+++ Week1/task5.java
@@ -2,6 +2,13 @@ package Task.Week1;
 
 public class task5 {
     public static void main(String[] args){
+        
+        /*WAP prints the following pattern:
+         *********
+         *********
+         *********
+         ********* */
+
         System.out.print("********* \n");
         System.out.print("********* \n");
         System.out.print("********* \n");
